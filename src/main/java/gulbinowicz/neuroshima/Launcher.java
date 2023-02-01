8d4ab7f33d1f4ed9ab755b91a8e4b986a772077c
Kuba <jakub.gulbinowicz@gmail.com>
2023-02-01 17:29:18 +0100

    GameEngine implemented

--- src/main/java/gulbinowicz/neuroshima/Launcher.java
+++ src/main/java/gulbinowicz/neuroshima/Launcher.java
@@ -1,7 +1,17 @@
 package gulbinowicz.neuroshima;
 
+import gulbinowicz.neuroshima.core.Display;
+import gulbinowicz.neuroshima.core.GameEngine;
+import gulbinowicz.neuroshima.game.Gameplay;
+
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Display display = new Display("NeuroshimaHex");
+        Gameplay gameplay = new Gameplay();
+        GameEngine gameEngine = new GameEngine(display, gameplay);
+        gameEngine.start();
+
     }
+
 }
---
+++ src/main/java/gulbinowicz/neuroshima/game/Gameplay.java
@@ -0,0 +1,13 @@
+package gulbinowicz.neuroshima.game;
+
+import java.awt.*;
+
+public class Gameplay {
+    public void tick(){
+        System.out.println("tick");
+    }
+    public void render(Graphics graphics){
+        System.out.println("render");
+
+    }
+}
