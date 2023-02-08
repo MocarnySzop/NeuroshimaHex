4b17e2345ddd5104cf6e530176754a4b5d5d9972
Kuba <jakub.gulbinowicz@gmail.com>
2023-02-08 17:51:49 +0100

    add game graphics

--- src/main/java/gulbinowicz/neuroshima/game/Gameplay.java
+++ src/main/java/gulbinowicz/neuroshima/game/Gameplay.java
@@ -2,7 +2,9 @@ package gulbinowicz.neuroshima.game;
 
 
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
 
 import static gulbinowicz.neuroshima.core.Display.*;
 
@@ -14,11 +16,22 @@ public class Gameplay {
 
     public void render(Graphics g) {
 
-
-        g.drawRect(getWidth(0.09),getHeight(0.09), getWidth(0.83), getHeight(0.67));
-        g.drawRect(getWidth(0.09),getHeight(0.80), getWidth(0.83), getHeight(0.167));
-        g.drawRect(getWidth(0.1), getHeight(0.1), getWidth(0.09), getHeight(0.09));
-        g.drawRect(getWidth(0.1), getHeight(0.66), getWidth(0.09), getHeight(0.09));
+        try {
+            g.drawImage(ImageIO.read(Gameplay.class.getResource("/board.jpg")),getWidth(0.09),getHeight(0.09), getWidth(0.83), getHeight(0.67),null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            g.drawImage(ImageIO.read(Gameplay.class.getResource("/hex.jpg")),getWidth(0.09),getHeight(0.80), getWidth(0.83), getHeight(0.167),null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        g.drawRect(getWidth(0.09),getHeight(0.80), getWidth(0.83), getHeight(0.167));
+        g.setColor(Color.MAGENTA);
+        g.drawString("Enemy = 10",getWidth(0.1), getHeight(0.15));
+//        g.drawRect(getWidth(0.1), getHeight(0.1), getWidth(0.09), getHeight(0.09));
+//        g.drawRect(getWidth(0.1), getHeight(0.66), getWidth(0.09), getHeight(0.09));
+        g.drawString("Player = 10",getWidth(0.1), getHeight(0.70));
 
 //g.drawLine( 1, 1, 600, 600);
 //        g.drawLine(1, 600, 600, 1);
